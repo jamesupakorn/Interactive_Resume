ab8b97278039b637978e859e6cbe3156aed67695
Supakorn <Supakorn.r@entronica.co.th>
2026-04-02 17:10:02 +0700

    fix: add React default import for Component class support

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Component, Fragment, Suspense, useEffect, useMemo, useRef, useState } from "react";
+import React, { Component, Fragment, Suspense, useEffect, useMemo, useRef, useState } from "react";
 import { Html, useAnimations, useFBX, useGLTF } from "@react-three/drei";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { clone } from "three/examples/jsm/utils/SkeletonUtils.js";
